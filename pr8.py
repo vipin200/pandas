64b4313ffda94cd95ba608393fba639c992b7493
vipin200 <vipin2000kaushik@gmail.com>
2020-01-22 13:13:50 +0530

    Q9

--- pr8.py
+++ pr8.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.DataFrame([[0 ,1 ,4 ,7],[1 ,2 ,5 ,5],[2 ,3, 6, 8],[3, 4, 9 ,12],[4 ,7 ,5 ,1],[5 ,11 ,0 ,11]], )
+df = pd.DataFrame([[0 ,1 ,4 ,7],[1 ,2 ,5 ,5],[2 ,3, 6, 8],[3, 4, 9, 12],[4, 7 ,5 ,1],[5 ,11 ,0 ,11]], columns=list("ABCD") )
+print(df)
+arr = pd.Series(df['A'])
+print(arr)
+
 
---
+++ pr9.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+t1 = pd.Series([2, 4, 6, 8, 10] )
+arr = np.array(t1)
+print(arr)
